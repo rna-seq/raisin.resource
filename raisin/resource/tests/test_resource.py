bc00b7be184fa83149da82c490bb6c3c25fd72e5
Maik R��der <maikroeder@gmail.com>
2011-07-09 14:19:04 +0000

    =pep8

--- raisin/resource/tests/test_resource.py
+++ raisin/resource/tests/test_resource.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 from raisin.resource import root
 
-class ResourceTest(unittest.TestCase):    
+class ResourceTest(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
 
